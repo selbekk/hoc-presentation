eff7dbb68038082fe0941985d6872dea19843a21
Kristofer Selbekk <selbeezy@gmail.com>
2017-02-28 19:08:48 +0100

    Add a fake API

---
+++ src/api/server.js
@@ -0,0 +1,11 @@
+const delayResponse = response => new Promise(resolve => setTimeout(() => {
+  resolve(response);
+}, 1000));
+
+export const getTodos = () => {
+  return delayResponse([
+    { id: '1', text: 'Gå på BEKK Open', done: true },
+    { id: '2', text: 'Lær om higher order components', done: false },
+    { id: '3', text: 'Lek rockestjerne med BEKK Band', done: false },
+  ]);
+}
